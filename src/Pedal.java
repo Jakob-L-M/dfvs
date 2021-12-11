b57b3690d006664fa8a0bd04b32900922cc425ce
Jakob-L-M <jakob-151@web.de>
2021-12-11 20:39:37 +0100

    highspeed increase
    Former-commit-id: 14f48b993bf1378c1371459fe5822707af4a49ac

--- src/Pedal.java
+++ src/Pedal.java
@@ -2,80 +2,52 @@ import java.util.*;
 
 public class Pedal {
 
-    public static int findDisjointPaths(Map<Integer, DirectedNode> nodeMap, int s, int t) {
-        int u, v;
-
+    public static int findDisjointPaths(Map<Integer, DirectedNode> nodeMap, int source, int sink) {
         // Residual graph where rGraph[i][j] indicates
         // residual capacity of edge from i to j (if there
         // is an edge. If rGraph[i][j] is 0, then there is not)
-        Map<Integer, Map<Integer, Integer>> rGraph = new HashMap<>();
+        Map<Integer, Set<Integer>> rGraph = new HashMap<>();
         Map<Integer, Integer> parent = new HashMap<>();
 
         for (Integer node : nodeMap.keySet()) {
-            rGraph.put(node, new HashMap<>());
-            for (Integer outNode : nodeMap.get(node).getOutNodes()) {
-                rGraph.get(node).put(outNode, 1);
-            }
-            parent.put(node, 0);
+            rGraph.put(node, new HashSet<>(nodeMap.get(node).getOutNodes()));
         }
 
-        int max_flow = 0; // There is no flow initially
+        int max_flow = 0;
 
-        // Augment the flow while there is path
-        // from source to sink
-        while (bfs(rGraph, s, t, parent)) {
-            // Find minimum residual capacity of the edges
-            // along the path filled by BFS. Or we can say
-            // find the maximum flow through the path found.
-            int path_flow = Integer.MAX_VALUE;
-
-            for (v = t; v != s; v = parent.get(v)) {
-                u = parent.get(v);
-                path_flow = Math.min(path_flow, rGraph.get(u).containsKey(v) ? 1 : 0);
+        while (bfs(rGraph, source, sink, parent)) {
+            int cur = parent.get(sink);
+            while (cur != source) {
+                rGraph.remove(cur);
+                cur = parent.get(cur);
             }
-
-            // update residual capacities of the edges and
-            // reverse edges along the path
-            for (v = t; v != s; v = parent.get(v)) {
-                u = parent.get(v);
-                rGraph.get(u).put(v, rGraph.get(u).get(v) - path_flow);
-                rGraph.get(v).put(u, (rGraph.get(v).containsKey(u) ? 1 : 0) + path_flow);
-            }
-
-            // Add path flow to overall flow
-            max_flow += path_flow;
+            max_flow += 1;
+            parent.clear();
         }
-
-        // Return the overall flow (max_flow is equal to
-        // maximum number of edge-disjoint paths)
-
         return max_flow;
     }
 
-    private static boolean bfs(Map<Integer, Map<Integer, Integer>> rGraph, int s, int t, Map<Integer, Integer> parent) {
-        // Create a visited array and
-        // mark all vertices as not visited
+    private static boolean bfs(Map<Integer, Set<Integer>> rGraph, int source, int sink, Map<Integer, Integer> parent) {
+        Set<Integer> visited = new HashSet<>();
+        Queue<Integer> queue = new LinkedList<>();
 
-        Map<Integer, Boolean> visited = new HashMap<>();
-
-
-        // Create a queue, enqueue source vertex and
-        // mark source vertex as visited
-        Queue<Integer> q = new LinkedList<>();
-        q.add(s);
-        visited.put(s, true);
-        parent.put(s, -1);
+        queue.add(source);
+        visited.add(source);
 
         // Standard BFS Loop
-        while (!q.isEmpty()) {
-            int u = q.peek();
-            q.remove();
+        while (!queue.isEmpty()) {
+            int current = queue.remove();
 
-            for (int v : rGraph.get(u).keySet()) {
-                if (!visited.containsKey(v) && rGraph.get(u).get(v) > 0) {
-                    q.add(v);
-                    parent.put(v, u);
-                    visited.put(v, true);
+            if (rGraph.containsKey(current)) {
+                for (int outNode : rGraph.get(current)) {
+                    if (!visited.contains(outNode)) {
+                        parent.put(outNode, current);
+                        if (outNode == sink) {
+                            return true;
+                        }
+                        queue.add(outNode);
+                        visited.add(outNode);
+                    }
                 }
             }
         }
@@ -83,6 +55,6 @@ public class Pedal {
         // If we reached sink in BFS
         // starting from source, then
         // return true, else false
-        return (visited.containsKey(t));
+        return visited.contains(sink);
     }
 }
