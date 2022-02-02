3777333f7731eb5409e993bf0db1b888df5e9aed
Jakob-L-M <jakob-151@web.de>
2022-02-02 23:36:53 +0100

    using stack for petals
    Former-commit-id: 01e2e2349c0a8482f7c24a59141fa9eaa03950a4

--- src/Petal.java
+++ src/Petal.java
@@ -2,63 +2,51 @@ import java.util.*;
 
 public class Petal {
 
-    public static Tuple getPetalSet(DirectedGraph graph, int nodeId, int limit) {
+    public static Tuple getPetalSet(DirectedGraph graph, int nodeId, int limit, boolean quick) {
         graph.addNode(-1); //source
         graph.addNode(-2); //sink
 
         DirectedNode node = graph.nodeMap.get(nodeId);
 
         for (Integer outNode : node.getOutNodes()) {
-            graph.addEdge(-1, outNode, false);
+            graph.addEdge(-1, outNode);
         }
 
         for (Integer inNode : node.getInNodes()) {
-            graph.addEdge(inNode, -2, false);
+            graph.addEdge(inNode, -2);
         }
 
-        graph.removeNode(nodeId, false);
+        graph.addStackCheckpoint();
 
-        Map<Integer, Set<Integer>> rGraph = new HashMap<>();
+        graph.removeNode(nodeId);
+
         Map<Integer, Integer> parent = new HashMap<>();
         Set<Integer> petalSet = new HashSet<>();
 
-        for (Integer nodes : graph.nodeMap.keySet()) {
-            rGraph.put(nodes, graph.nodeMap.get(nodes).getOutNodes());
-        }
-
         int max_flow = 0;
 
-        while (bfs(rGraph, parent, limit)) {
+        while (bfs(graph, parent, limit)) {
             int cur = parent.get(-2);
             while (cur != -1) {
-                rGraph.remove(cur);
-                petalSet.add(cur);
+                graph.removeNode(cur);
+                if(!quick) petalSet.add(cur);
                 cur = parent.get(cur);
             }
             max_flow += 1;
             parent.clear();
         }
 
-        graph.addNode(nodeId);
-
-        for (Integer outNode : node.getOutNodes()) {
-            graph.addEdge(nodeId, outNode, false);
-        }
-
-        for (Integer inNode : node.getInNodes()) {
-            graph.addEdge(inNode, nodeId, false);
-        }
-
+        graph.rebuildGraph();
         graph.removeNode(-1, false);
         graph.removeNode(-2, false);
 
-        graph.nodeMap.get(nodeId).setPedal(max_flow);
+        if(!quick)graph.nodeMap.get(nodeId).setPedal(max_flow);
 
         petalSet.add(nodeId);
         return new Tuple(max_flow, petalSet);
     }
 
-    private static boolean bfs(Map<Integer, Set<Integer>> rGraph, Map<Integer, Integer> parent, int limit) {
+    private static boolean bfs(DirectedGraph graph, Map<Integer, Integer> parent, int limit) {
         Set<Integer> visited = new HashSet<>();
         Queue<Integer> queue = new LinkedList<>();
 
@@ -68,8 +56,8 @@ public class Petal {
         while (!queue.isEmpty()) {
             int current = queue.remove();
 
-            if (rGraph.containsKey(current)) {
-                for (int outNode : rGraph.get(current)) {
+            if (graph.nodeMap.containsKey(current)) {
+                for (int outNode : graph.nodeMap.get(current).getOutNodes()) {
                     if (!visited.contains(outNode)) {
                         parent.put(outNode, current);
                         // reached sink - break here
