7653b3280c403dfcf65dcf7d33a28f4b23e23be1
Jakob-L-M <jakob-151@web.de>
2022-02-11 16:57:11 +0100

    tracking classes
    Former-commit-id: 2ac3c2e058fa3983070b2354581cd98cfd13ee17

---
+++ src/Timer.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Timer {
+
+    public long[] times;
+    private final Map<String, Integer> timeMap;
+
+    public Timer(int slots, String[] names) {
+        assert slots == names.length;
+        times = new long[slots];
+        timeMap = new HashMap<>();
+        int c = 0;
+        for (String s : names) {
+            timeMap.put(s, c);
+            c++;
+        }
+    }
+
+    public void addTime(String name, long time) {
+        times[timeMap.get(name)] += time;
+    }
+
+    public void addTimer(Timer that) {
+        assert that.times.length == this.times.length;
+        for (int i = 0; i < times.length; i++) {
+            this.times[i] += that.times[i];
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        long total = Arrays.stream(times).sum();
+        res.append("time: ").append(utils.round((double)total/1_000_000_000, 4)).append(" ");
+        for (String name : timeMap.keySet()) {
+            res.append(name).append(": ");
+            double percent = utils.round((double) 100 * times[timeMap.get(name)]/total, 4);
+            res.append(percent).append("% | ");
+        }
+        return res.toString();
+    }
+}
---
+++ src/TimerTuple.java
@@ -0,0 +1,20 @@
+import java.util.Set;
+
+public class TimerTuple {
+
+    private final Timer timer;
+    private final Set<Integer> set;
+
+    public TimerTuple(Timer timer, Set<Integer> set) {
+        this.timer = timer;
+        this.set = set;
+    }
+
+    public Set<Integer> getSolution() {
+        return set;
+    }
+
+    public Timer getTimer() {
+        return timer;
+    }
+}
